2dc65ae877f46bfd35f86b7a3047222f8308cc97
Ufon <ufon.exe@yandex.ru>
2018-03-23 18:46:12 +0300

    Fix warnings.

--- src/components/Count/index.js
+++ src/components/Count/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-firebase';
 
-const Counter = ({ value, setValue, uuid }) => <span>{value}</span>;
+const Counter = ({ value }) => <span>{value}</span>;
 
 Counter.propTypes = {
-  uuid: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
-  setValue: PropTypes.func.isRequired,
 };
 
-const mapFirebaseToProps = (props, ref) => ({
-  value: props.uuid,
-  setValue: value => ref(`likes/${props.uuid}`).set(value),
-});
-
-export default connect(mapFirebaseToProps)(Counter);
+export default Counter;
--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Count } from 'components';
 import styles from './styles.scss';
 
 class LikeButton extends Component {
